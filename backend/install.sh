a374af96f9e303b16be14015aa457f3f7f806a45
EC2 Default User <ec2-user@ip-172-31-32-38.ap-northeast-1.compute.internal>
2020-09-28 11:13:31 +0000

    環境構築に必要なパッケージのインストールコマンドをまとめたシェルスクリプトの追加

---
+++ backend/install.sh
@@ -0,0 +1,19 @@
+sudo yum update -y
+sudo yum install -y git
+
+# Docker
+sudo yum install -y docker git
+sudo service docker start
+sudo usermod -a -G docker ec2-user  # Docker の実行に sudo いらなくなる
+
+# Docker Compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Python
+sudo yum install python3 -y
+cd ~/trigger
+python3 -m venv venv
+source venv/bin/activate
+pip install poetry
