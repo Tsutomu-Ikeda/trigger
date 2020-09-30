564c65b31fd8eaa00f571b808526b6d30a0696b9
tomtsutom <tomtsutom0122@gmail.com>
2020-09-30 16:01:11 +0900

    ログイン済みであればログイン画面を表示しないようにした

--- front/src/views/SignIn.tsx
+++ front/src/views/SignIn.tsx
@@ -6,10 +6,11 @@ import {
   LogIn,
 } from "store/actions";
 import { useQuery } from "libs/Url";
+import { Redirect } from "react-router-dom";
 
 export default function SignIn() {
   const {
-    dispatch
+    state, dispatch
   } = useContext(store);
   const query = useQuery();
 
@@ -19,19 +20,17 @@ export default function SignIn() {
       window.location.href = query.get("redirect_to") || "/";
     }, [dispatch, query]);
 
-  return (
-    <div>
-      <Typography color="inherit" variant="h5" component="h2">
-        ログイン画面
-     </Typography>
+  return state.isAuthenticated ? <Redirect to={query.get("redirect_to") || "/"} /> : <>
+    <Typography color="inherit" variant="h5" component="h2">
+      ログイン画面
+        </Typography>
 
-      <button
-        type="button"
-        onClick={logIn}
-        style={{ margin: "10px 0", float: "right" }}
-      >
-        ログイン
-      </button>
-    </div>
-  );
+    <button
+      type="button"
+      onClick={logIn}
+      style={{ margin: "10px 0", float: "right" }}
+    >
+      ログイン
+        </button>
+  </>;
 }
