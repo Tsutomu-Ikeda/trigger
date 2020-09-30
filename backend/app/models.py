267c4ce8ea5d6b6a1423800917f89545556f3cb1
Koki Nishihara <snkg2450@gmail.com>
2020-09-30 16:41:06 +0900

    model:Baseをdb.Modelに修正。コメント削除

--- backend/app/models.py
+++ backend/app/models.py
@@ -11,7 +11,7 @@ db = SQLAlchemy()
 Base = declarative_base()
 
 
-class Student(Base):
+class Student(db.Model):
     __tablename__ = "students"
 
     id = db.Column(db.Integer, primary_key=True)
@@ -22,15 +22,12 @@ class Student(Base):
     passowrd = db.Column(db.String(120), unique=False, nullable=False)
     certificate_url = db.Column(db.String(120), unique=True, nullable=False)  # 照明
     user_type = db.Column(db.String(20), unique=False, nullable=False)
-    # affiliation = db.relationship(
-    #     "Universities", backref="students", lazy=True
-    # )  # 所属, TODO relation の整備
     affiliation_id = db.Column(db.Integer, db.ForeignKey("universities.id"))
     is_authenticated = db.Column(
         db.Boolean, unique=False, nullable=False, default=False
     )
-    type_card = db.Column(db.String(120), unique=True, nullable=True)
-    identity_card = db.Column(db.String(120), unique=True, nullable=True)
+    type_card_url = db.Column(db.String(120), unique=True, nullable=True)
+    identity_card_url = db.Column(db.String(120), unique=True, nullable=True)
     created_at = db.Column(
         Timestamp, unique=False, nullable=False, default=datetime.utcnow
     )
@@ -39,65 +36,56 @@ class Student(Base):
     )
 
 
-class Worker(Base):
+class Worker(db.Model):
     __tablename__ = "workers"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=False, nullable=False)
-    date_of_birth = db.Column(db.DateTime, unique=False, nullable=False)
+    date_of_birth = db.Column(db.Date, unique=False, nullable=False)
     tel_number = db.Column(db.String(11), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     passowrd = db.Column(db.String(120), unique=False, nullable=False)
     certificate_url = db.Column(db.String(120), unique=True, nullable=False)  # 照明
     user_type = db.Column(db.String(20), unique=False, nullable=False)
-    # affiliation = db.relationship(
-    #     "Companies", backref="workers", lazy=True
-    # )  # 所属, TODO relation の整備
     affiliation_id = db.Column(db.Integer, db.ForeignKey("companies.id"))
-    # job = db.relationship("Jobs", backref="workers", lazy=True)  # TODO relation の整備
     job = db.Column(db.Integer, db.ForeignKey("jobs.id"))
-    # department = db.relationship(
-    #     "Departments", backref="workers", lazy=True
-    # )  # 部署, TODO relation の整備
-    # position = db.relationship(
-    #     "Positions", backref="workers", lazy=True
-    # )  # 役職, TODO relation の整備
     department = db.Column(db.String(120), unique=False, nullable=True)
     postions = db.Column(db.String(120), unique=False, nullable=True)
     is_authenticated = db.Column(
         db.Boolean, unique=False, nullable=False, default=False
     )
-    type_card = db.Column(db.String(120), unique=True, nullable=True)
-    identity_card = db.Column(db.String(120), unique=True, nullable=True)
+    type_card_url = db.Column(db.String(120), unique=True, nullable=True)
+    identity_card_url = db.Column(db.String(120), unique=True, nullable=True)
     created_at = db.Column(
-        Timestamp, unique=False, nullable=False, default=datetime.utcnow
+        Timestamp, unique=False, nullable=False, default=datetime.now
     )
     cupdated_at = db.Column(
-        Timestamp, unique=False, nullable=False, default=datetime.utcnow
+        Timestamp, unique=False, nullable=False, default=datetime.now
     )
     comment = db.Column(db.String(80), unique=False, nullable=True)
 
 
-class University(Base):
+class University(db.Model):
     __tablename__ = "universities"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=False, nullable=False)
 
 
-class Company(Base):
+class Company(db.Model):
     __tablename__ = "companies"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=False, nullable=False)
 
 
-class Job(Base):
+class Job(db.Model):
     __tablename__ = "jobs"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=False, nullable=False)
 
 
-class Match(Base):
+class Match(db.Model):
     __tablename__ = "matches"
+    
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.DateTime, unique=False, nullable=True)
     speaker_id = db.Column(db.Integer, db.ForeignKey("workers.id"))
