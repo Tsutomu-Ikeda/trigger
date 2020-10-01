a3424dabbde046d397aa08ff3b41e5344070b135
nukopy <pytwbf201830@gmail.com>
2020-10-01 17:46:49 +0900

    fix: import error

--- backend/app/services/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-class Base:
-    def __init__(self):
-        self.access_key = os.getenv("AWS_ACCESS_KEY")
-        self.secret_key = os.getenv("AWS_SECRET_KEY")
-        self.region = os.getenv("AWS_REGION")
-        self.email = os.getenv("AWS_EMAIL")
