510dadfd544e37afba88e3f27568efb27447d248
tomtsutom <tomtsutom0122@gmail.com>
2020-10-01 19:36:40 +0900

    匿名音声通話実装

--- front/src/views/Matching/Call.tsx
+++ front/src/views/Matching/Call.tsx
@@ -1,60 +1,96 @@
 import React, { useEffect, useRef, useState } from "react";
-// import * as Tone from "tone";
+import * as Tone from "tone";
 import Peer from 'skyway-js';
 
 import AuthRequired from "components/AuthRequired";
-// import { useParams } from "libs/Url";
+import { useParams } from "libs/Url";
+import { getMatchingDetail } from "libs/ServerClient";
 
-const peer = new Peer({
+const createPeer = () => (new Peer({
   key: "b6b6bf8d-c7d2-4b6a-bba5-cd527c9faa08",
   debug: 3,
-});
+}));
 
 export default function Call() {
+  const { id: matchingId } = useParams() as { id: string };
   const [myId, setMyId] = useState('')
   const [callId, setCallId] = useState('')
-  const localVideo = useRef<HTMLVideoElement>(null)
-  const remoteVideo = useRef<HTMLVideoElement>(null)
+  const localVideo = useRef<HTMLVideoElement>(null);
+  const remoteVideo = useRef<HTMLVideoElement>(null);
+  const [peer, setPeer] = useState(null as Peer | null);
+  const micAudio = new Tone.UserMedia();
+  const matching = getMatchingDetail(matchingId);
+
+  useEffect(() => {
+    setPeer(createPeer());
+  }, []);
 
   useEffect(
     () => {
-      if (navigator.mediaDevices && localVideo) {
+      if (navigator.mediaDevices && peer) {
         peer.on('open', () => {
-          setMyId(peer.id)
-          navigator.mediaDevices.getUserMedia({ video: false, audio: true }).then(localStream => {
-            (localVideo.current as any).srcObject = localStream
-          })
+          setMyId(peer.id);
         })
 
         peer.on('call', mediaConnection => {
           mediaConnection.answer((localVideo.current as any).srcObject)
 
-          mediaConnection.on('stream', async stream => {
-            (remoteVideo.current as any).srcObject = stream
+          mediaConnection.on('stream', async (stream) => {
+            (remoteVideo.current as any).srcObject = stream;
           })
         })
       }
-    }, [localVideo]
+    }, [peer]
   )
+  micAudio.open().then(() => {
+    const shifter = new Tone.PitchShift(5);
+    const reverb = new Tone.Freeverb();
+    // 加工済みの音声を受け取る空のノードを用意
+    const effectedDest = Tone.context.createMediaStreamDestination();
+    micAudio.connect(shifter);
+    shifter.connect(reverb);
+    // リバーブを空のノードに接続
+    reverb.connect(effectedDest);
+
+    // カメラ映像取得
+    navigator.mediaDevices.getUserMedia({ video: false, audio: true })
+      .then(stream => {
+
+        // ストリームから音声トラックを削除
+        const oldTrack = stream.getAudioTracks()[0];
+        stream.removeTrack(oldTrack);
+
+        // ストリームにエフェクトがかかった音声トラックを追加
+        const effectedTrack = effectedDest.stream.getAudioTracks()[0];
+        stream.addTrack(effectedTrack);
+
+        (localVideo.current as any).srcObject = stream;
+      }).catch(error => {
+        // 失敗時にはエラーログを出力
+        console.error('mediaDevice.getUserMedia() error:', error);
+        return;
+      });
+  });
 
   const makeCall = () => {
-    const mediaConnection = peer.call(callId, (localVideo.current as any).srcObject)
-    mediaConnection.on('stream', async stream => {
-      (remoteVideo.current as any).srcObject = stream
-      await (remoteVideo.current as any).play().catch(console.error)
-    })
+    if (peer) {
+      const mediaConnection = peer.call(callId, (localVideo.current as any).srcObject)
+      mediaConnection.on('stream', async (stream) => {
+        (remoteVideo.current as any).srcObject = stream
+      })
+    }
   }
   return (
     <AuthRequired>
       <div>
-        <div>skyway test</div>
-        <div><video width="400px" autoPlay muted playsInline ref={localVideo}></video></div>
+        <h3>{matching.speaker.companyName}</h3>
+        <div><video width="400px" hidden autoPlay muted playsInline ref={localVideo}></video></div>
         <div>{myId}</div>
         <div>
           <input value={callId} onChange={e => setCallId(e.target.value)}></input>
           <button onClick={makeCall}>発信</button>
         </div>
-        <div><video width="400px" autoPlay muted playsInline ref={remoteVideo}></video></div>
+        <div><video width="400px" autoPlay playsInline ref={remoteVideo}></video></div>
       </div>
     </AuthRequired>
   )
